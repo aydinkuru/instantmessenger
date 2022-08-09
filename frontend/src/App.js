28d917494c954b6a0c595e06c657da93e056b5d0
aydinkuru <aydinkuru81@gmail.com>
2022-08-09 22:36:16 +0200

    react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import { Button } from "@chakra-ui/react";
 import "./App.css";
+import { Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme="red">Button</Button>
+      <Route path="/" component={Homepage} />
+      <Route path="/chats" component={ChatPage} />
     </div>
   );
 }

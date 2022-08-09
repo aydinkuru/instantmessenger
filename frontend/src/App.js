ae5fca01c12d1f39a466f3fbefbebdaae493d12f
aydinkuru <aydinkuru81@gmail.com>
2022-08-09 21:17:07 +0200

    arg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Button colorScheme="red">Button</Button>
+    </div>
+  );
 }
 
 export default App;

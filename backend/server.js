23ada78f9b427bd39ea2f60d89a8737399302bfb
aydinkuru <aydinkuru81@gmail.com>
2022-08-11 23:59:32 +0200

    routes

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,7 @@ app.get("/", (req, res) => {
   res.send("API is Running Successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 

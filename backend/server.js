0c5e4dcdddd9220e8b7f0bee3c8b1dfd45e42fd5
aydinkuru <aydinkuru81@gmail.com>
2022-08-13 01:34:53 +0200

    json web token

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
+
 connectDB();
 const app = express();
+
+app.use(express.json()); // to accept JSON Data
 
 app.get("/", (req, res) => {
   res.send("API is Running Successfully");

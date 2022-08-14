009e4c1f855a68056e3f1f932fd60d13debf1d58
aydinkuru <aydinkuru81@gmail.com>
2022-08-14 18:52:20 +0200

    password encryption with bcrypt

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,22 +39,21 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
-    if (user && ()) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            pic: user.pic,
-            token: generateToken(user._id),
-        });
-    } else {
-        res.status(401);
-        throw new Error("Invalid Email or Password");
-    }
-
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
 });
 
-module.exports = { registerUser };
+module.exports = { registerUser, authUser };

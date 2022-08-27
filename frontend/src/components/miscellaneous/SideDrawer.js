0572153b0f9fa072b89847af0db32f55a8369a51
aydinkuru <aydinkuru81@gmail.com>
2022-08-27 22:50:41 +0200

    side draver

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -4,7 +4,7 @@ const SideDrawer = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [loadingChat, setLoadingChat] = useState();
   return <div>SideDrawer</div>;
 };
 

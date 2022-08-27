c643c0cc58c854081b15e4dd5e1b3b17bcd79ee5
aydinkuru <aydinkuru81@gmail.com>
2022-08-27 22:48:51 +0200

    s4.3

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return <div>SideDrawer</div>;
 };
 
